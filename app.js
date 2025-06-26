e7d52f3dcd292f83c9cba62e5caa960f170e16f9
Musisi <musisironald2025@gmail.com>
2025-06-26 23:44:05 +0300

    version 2 to test full stack

--- app.js
+++ app.js
@@ -1,17 +1,64 @@
 const express = require ('express');
 const cors = require ('cors')
-const port = 8000;
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const port = process.env.SERVER_PORT
 const app = express();
+const { sequelize, testConnection } = require('./db');
+const { QueryTypes } = require('sequelize');
 
 app.use(cors());
-app.get('/home', (req, res) => {
-  res.status(200).json({
-    make: 'Toyota',
-    design: 'Station Wagon',
-    tradeName: 'Wish'
-  })
-} )
+
+app.get('/cars', (req, res) => {
+  res.status(200).json(
+    [
+      {name: 'Wish'},
+      {name: 'LandCruiser'},
+      {name: 'Volvo'},
+      {name: 'Beast'}
+    ]
+  )
+} );
+
+//function to fetch fruits from mytest db
+const getAllFruits = async(req, res) => {
+  try {
+    const response = await sequelize.query(
+      `SELECT fruitName AS name FROM Fruit`,
+      {
+        type: QueryTypes.SELECT
+      }
+    );
+    if(response && response.length>0) {
+      console.log(response);
+      return res.status(200).json({
+        message: 'All fruits successifully retrieved',
+        fruits: response
+      })
+    };
+
+    if(!response || !response.length>0) {
+      console.log('No fuit data was found');
+      return res.status(404).json({
+        message: 'No fruits were found',
+        fruits: {}
+      })
+    }
+  } catch (error) {
+    console.log('Error while fetching fruit data: ', error);
+      return res.status(500).json({
+        message: 'A error occured while retrieving fruit data',
+        fruits: {}
+      })
+  }
+}
+
+app.get('/fruits', getAllFruits)
+
 app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+testConnection();
 
 
 module.exports = app;
